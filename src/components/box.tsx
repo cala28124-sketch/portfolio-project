705db66d35fadf6be212284ec0b9119637af9501
Anthony Calabrese <cala28124@gmail.com>
2025-10-21 17:15:12 -0400

    Added secondary boxes

--- src/components/box.tsx
+++ src/components/box.tsx
@@ -4,11 +4,13 @@ const Box = () => {
   return (
     <>
       <div className="d-flex justify-content-center align-items-center vh-100">
+        <div className="bg-secondary bg-white text-black rounded-3 box-left shadow "></div>
         <div className="bg-secondary bg-white text-black rounded-3 w-50 h-50 shadow ">
           <div className="box-text">
             this is my super awesome cool background box!!!
           </div>
         </div>
+        <div className="bg-secondary bg-white text-black rounded-3 box-right shadow "></div>
       </div>
     </>
   );
