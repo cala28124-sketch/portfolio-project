5a18d7ce735abc989db981a28a9bfc8e674ad201
Anthony Calabrese <cala28124@gmail.com>
2025-10-22 19:04:40 -0400

    added navbar layout. tested audio and image functions.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,11 +3,90 @@ import React from "react";
 const Navbar = () => {
   return (
     <>
-      <nav className="navbar bg-secondary">
+      <nav className="navbar navbar-expand-lg color_box">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            <div className="navbar-text">test</div>
+          <a
+            className="navbar-brand navbar-image-initial"
+            href="https://www.coolmathgames.com/"
+          >
+            <img
+              className="navbar-logo"
+              src="/test_logo.webp"
+              alt="shhhh silly"
+            />
           </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <a
+                className="nav-link active navbar-space-initial"
+                aria-current="page"
+                style={{ color: "white" }}
+                href="#"
+              >
+                About
+              </a>
+              <a
+                className="nav-link nav-link navbar-space"
+                style={{ color: "white" }}
+                href="#"
+              >
+                Career
+              </a>
+              <a
+                className="nav-link nav-link navbar-space"
+                style={{ color: "white" }}
+                href="#"
+              >
+                Project
+              </a>
+              <a
+                className="nav-link nav-link navbar-space"
+                style={{ color: "white" }}
+                href="#"
+              >
+                Skills
+              </a>
+              <a
+                className="nav-link navbar-space"
+                style={{ color: "white" }}
+                href="#"
+              >
+                Featured
+              </a>
+              <a className="navbar-brand navbar-image-space" href="linked in">
+                <img
+                  className="navbar-logo"
+                  src="/test_logo.webp"
+                  alt="shhhh silly"
+                />
+              </a>
+              <a className="navbar-brand" href="github">
+                <img
+                  className="navbar-logo"
+                  src="/test_logo.webp"
+                  alt="shhhh silly"
+                />
+              </a>
+              <a className="navbar-brand" href="resume">
+                <img
+                  className="navbar-logo"
+                  src="/test_logo.webp"
+                  alt="shhhh silly"
+                />
+              </a>
+            </div>
+          </div>
         </div>
       </nav>
     </>
--- src/components/box.tsx
+++ src/components/box.tsx
@@ -4,13 +4,22 @@ const Box = () => {
   return (
     <>
       <div className="d-flex justify-content-center align-items-center vh-100">
-        <div className="bg-secondary bg-white text-black rounded-3 box-left shadow "></div>
-        <div className="bg-secondary bg-white text-black rounded-3 w-50 h-50 shadow ">
+        <div className="color_box rounded-3 box-left shadow "></div>
+        <div className="color_box rounded-3 w-50 h-75 shadow ">
           <div className="box-text">
-            this is my super awesome cool background box!!!
+            <img
+              className="box-image"
+              src="/headshotme.jpg"
+              alt="shhhh silly"
+            />
           </div>
         </div>
-        <div className="bg-secondary bg-white text-black rounded-3 box-right shadow "></div>
+        <div className="color_box rounded-3 box-right shadow ">
+          <audio controls preload="auto">
+            <source src="/alicia.mp3" type="audio/mp3" />
+            Your browser does not support the audio element.
+          </audio>
+        </div>
       </div>
     </>
   );
