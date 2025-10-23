fa9b254f65683aa14fc9e4aded530123537479e3
Anthony Calabrese <cala28124@gmail.com>
2025-10-23 18:54:49 -0400

    Added video/use state for future plan

--- src/components/box.tsx
+++ src/components/box.tsx
@@ -1,24 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
 
 const Box = () => {
+  const [Videoshow, setStop] = useState(true);
+  const [Menushow, setMenu] = useState(false);
+  // might be some bugs here later since its a pure timeout? figure that out later
+  const timer = setTimeout(() => {
+    setStop(false);
+    setMenu(true);
+  }, 5000);
   return (
     <>
       <div className="d-flex justify-content-center align-items-center vh-100">
         <div className="color_box rounded-3 box-left shadow "></div>
         <div className="color_box rounded-3 w-50 h-75 shadow ">
           <div className="box-text">
-            <img
-              className="box-image"
-              src="/headshotme.jpg"
-              alt="shhhh silly"
-            />
+            {Videoshow && (
+              <video className="box-image" src="/hand1.mp4" autoPlay muted />
+            )}
+            {Menushow && (
+              <audio controls preload="auto" autoPlay>
+                <source src="/alicia.mp3" type="audio/mp3" />
+                Your browser does not support the audio element.
+              </audio>
+            )}
           </div>
         </div>
         <div className="color_box rounded-3 box-right shadow ">
-          <audio controls preload="auto">
-            <source src="/alicia.mp3" type="audio/mp3" />
-            Your browser does not support the audio element.
-          </audio>
+          {Videoshow && (
+            <video className="box-full" src="/hand2.mp4" autoPlay muted />
+          )}
         </div>
       </div>
     </>
