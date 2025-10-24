9cd42fd8dccda5f92c0c68469b0a3ed6fd681c29
Anthony Calabrese <cala28124@gmail.com>
2025-10-24 16:35:11 -0400

    updated navbar toggle

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,8 +3,12 @@ import React from "react";
 const Navbar = () => {
   return (
     <>
-      <nav className="navbar navbar-expand-lg color_box">
-        <div className="container-fluid">
+      <div
+        className="collapse"
+        id="navbarToggleExternalContent"
+        data-bs-theme="dark"
+      >
+        <div className="color_box p-4">
           <a
             className="navbar-brand navbar-image-initial"
             href="https://www.coolmathgames.com/"
@@ -15,78 +19,86 @@ const Navbar = () => {
               alt="shhhh silly"
             />
           </a>
+          <a
+            className="nav-text active navbar-space-initial"
+            aria-current="page"
+            href="#"
+          >
+            About
+          </a>
+          <a
+            className="nav-text active navbar-space"
+            aria-current="page"
+            href="#"
+          >
+            Career
+          </a>
+          <a
+            className="nav-text active navbar-space"
+            aria-current="page"
+            href="#"
+          >
+            Projects
+          </a>
+          <a
+            className="nav-text active navbar-space"
+            aria-current="page"
+            href="#"
+          >
+            Skill
+          </a>
+          <a
+            className="nav-text active navbar-space"
+            aria-current="page"
+            href="#"
+          >
+            featured
+          </a>
+          <a
+            className="navbar-brand navbar-image-space"
+            href="https://www.coolmathgames.com/"
+          >
+            <img
+              className="navbar-logo"
+              src="/test_logo.webp"
+              alt="shhhh silly"
+            />
+          </a>
+          <a
+            className="navbar-brand navbar-image"
+            href="https://www.coolmathgames.com/"
+          >
+            <img
+              className="navbar-logo"
+              src="/test_logo.webp"
+              alt="shhhh silly"
+            />
+          </a>
+          <a
+            className="navbar-brand navbar-image"
+            href="https://www.coolmathgames.com/"
+          >
+            <img
+              className="navbar-logo"
+              src="/test_logo.webp"
+              alt="shhhh silly"
+            />
+          </a>
+        </div>
+      </div>
+      <nav className="navbar navbar-dark body">
+        <div className="container-fluid">
           <button
             className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
-            data-bs-target="#navbarNavAltMarkup"
-            aria-controls="navbarNavAltMarkup"
+            data-bs-target="#navbarToggleExternalContent"
+            aria-controls="navbarToggleExternalContent"
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div className="navbar-nav">
-              <a
-                className="nav-link active navbar-space-initial"
-                aria-current="page"
-                style={{ color: "white" }}
-                href="#"
-              >
-                About
-              </a>
-              <a
-                className="nav-link nav-link navbar-space"
-                style={{ color: "white" }}
-                href="#"
-              >
-                Career
-              </a>
-              <a
-                className="nav-link nav-link navbar-space"
-                style={{ color: "white" }}
-                href="#"
-              >
-                Project
-              </a>
-              <a
-                className="nav-link nav-link navbar-space"
-                style={{ color: "white" }}
-                href="#"
-              >
-                Skills
-              </a>
-              <a
-                className="nav-link navbar-space"
-                style={{ color: "white" }}
-                href="#"
-              >
-                Featured
-              </a>
-              <a className="navbar-brand navbar-image-space" href="linked in">
-                <img
-                  className="navbar-logo"
-                  src="/test_logo.webp"
-                  alt="shhhh silly"
-                />
-              </a>
-              <a className="navbar-brand" href="github">
-                <img
-                  className="navbar-logo"
-                  src="/test_logo.webp"
-                  alt="shhhh silly"
-                />
-              </a>
-              <a className="navbar-brand" href="resume">
-                <img
-                  className="navbar-logo"
-                  src="/test_logo.webp"
-                  alt="shhhh silly"
-                />
-              </a>
-            </div>
-          </div>
         </div>
       </nav>
     </>
