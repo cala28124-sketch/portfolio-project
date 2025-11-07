bfa9824fc832034cfeef6776c707ec7cac0810b7
Anthony Calabrese <cala28124@gmail.com>
2025-11-07 18:35:54 -0500

    textbox expierementing

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
   };
 
   return (
-    <div>
+    <>
       <Navbar
         menuclick={menuClick}
         menuSkip={menuSkip}
@@ -29,7 +29,6 @@ function App() {
         setInteractiveText={setInteractiveText}
         setVideoshow={setStop}
       />
-
       <div className="d-flex justify-content-center align-items-center vh-100">
         <div className="color_box rounded-3 box-left shadow "></div>
         <div className="color_box rounded-3 w-50 h-50 shadow ">
@@ -72,7 +71,34 @@ function App() {
           )}
         </div>
       </div>
-    </div>
+      <div className="testbox">
+        <div className="viewport-text">
+          <h1>Welcome to My Portfolio</h1>
+          Overall, the activity I performed at this volunteering opportunity was
+          split into two parts- the set up, and the duration of the event. At
+          the beginning we had set up for the charity auction, setting tables,
+          putting out what was to be sold, etc. Afterwards, we helped out with
+          handling guests, and making sure the event ran smoothly. For this
+          event, I feel it was heavily worthwhile to go as I think Annabelle's
+          closet supports a wonderful cause. This is helping those facing
+          domestic abuse escape from their circumstances, and remake their life
+          once they have done so. I feel from this, as its one of my first
+          community service events I have gone to in my time here at UCF, I have
+          learned the importance of going out to help with community service,
+          and the good it can bring. At the same time it also feels fulfilling
+          to know that I was able to help, and assist an organization wanting to
+          do good in the world. At the end of the day, I feel it important to
+          keep in mind the good service you can bring to the community when
+          helping others, and how much it can mean to people when volunteers
+          come out to help them with activities like this.
+        </div>
+        <img
+          className="viewport-image"
+          src="headshotme.jpg"
+          alt="thats me fr"
+        />
+      </div>
+    </>
   );
 }
 
